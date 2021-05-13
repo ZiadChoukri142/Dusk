73f2569d7eb6e52a0edfa458cfa7ef7789dbc9b1
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-13 00:31:27 +0000

    added: includes - navbar

---
+++ includes/navbar.php
@@ -0,0 +1,15 @@
+<div class="navbar">
+                <div class="logo">
+                    <img src="./images/brand.png" width="130px" />
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="" class="active">Home</a></li>
+                        <li><a href="">Products</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Account</a></li>
+                    </ul>
+                </nav>
+                <img src="./images/cart.png" width="40px" />
+            </div>
