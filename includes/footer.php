88a3d2253cc753877637d88415eb9cca98139576
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-13 00:31:12 +0000

    added: includes - footer

---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+<footer class="footer-distributed">
+
+<div class="footer-left">
+
+    <p class="footer-links">
+        <a href="#">Products</a> <br>
+        <a href="#">About</a> <br>
+        <a href="#">Contact</a> <br>
+        <a href="#">Account</a>
+    </p>
+
+    <p class="footer-company-name">webdevtrick &copy; 2019</p>
+</div>
+
+<div class="footer-center">
+
+    <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>A19 Lazaret, Hay Salam</span> Oujda, Morocco</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        <p>+212605456188</p>
+    </div>
+
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:ziad.choukri@gmail.com">ziad.choukri@gmail.com</a></p>
+    </div>
+
+</div>
+
+<div class="footer-right">
+
+    <p class="footer-company-about">
+        <span>About the Business</span>
+        Dusk is a Worldwide E-commerce Website Specialized in selling High Quality Clothing and Accessories
+    </p>
+
+    <div class="footer-icons">
+
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-github"></i></a>
+
+    </div>
+
+</div>
+
+</footer>
+</html>
