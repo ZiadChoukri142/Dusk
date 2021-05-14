0f3c83aa8da8ee8d7a4d786d25d05f38c7d09d51
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-14 18:21:03 +0000

    modified: footer entirely to match the rest

--- includes/footer.php
+++ includes/footer.php
@@ -1,53 +1,32 @@
-<footer class="footer-distributed">
-
-<div class="footer-left">
-
-    <p class="footer-links">
-        <a href="#">Products</a> <br>
-        <a href="#">About</a> <br>
-        <a href="#">Contact</a> <br>
-        <a href="#">Account</a>
-    </p>
-
-    <p class="footer-company-name">webdevtrick &copy; 2019</p>
+<div class="footer">
+    <div class="containter">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>Download our App</h3>
+                <p>Download App for Android and IOS</p>
+            </div>
+            <div class="footer-col-2">
+                <img src="./static/images/logo-white.png">
+                <p>Our Purpose is to substainably make the pleasure and benefits of sports accessible to the many.</p>
+            </div>
+            <div class="footer-col-3">
+                <h3>Useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                        <li>Join Affiliate</li>
+                    </ul>
+            </div>
+            <div class="footer-col-4">
+                <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                        <li>Youtube</li>
+                    </ul>
+            </div>
+        </div>
+    </div>
 </div>
-
-<div class="footer-center">
-
-    <div>
-        <i class="fa fa-map-marker"></i>
-        <p><span>A19 Lazaret, Hay Salam</span> Oujda, Morocco</p>
-    </div>
-
-    <div>
-        <i class="fa fa-phone"></i>
-        <p>+212605456188</p>
-    </div>
-
-    <div>
-        <i class="fa fa-envelope"></i>
-        <p><a href="mailto:ziad.choukri@gmail.com">ziad.choukri@gmail.com</a></p>
-    </div>
-
-</div>
-
-<div class="footer-right">
-
-    <p class="footer-company-about">
-        <span>About the Business</span>
-        Dusk is a Worldwide E-commerce Website Specialized in selling High Quality Clothing and Accessories
-    </p>
-
-    <div class="footer-icons">
-
-        <a href="#"><i class="fa fa-facebook"></i></a>
-        <a href="#"><i class="fa fa-twitter"></i></a>
-        <a href="#"><i class="fa fa-linkedin"></i></a>
-        <a href="#"><i class="fa fa-github"></i></a>
-
-    </div>
-
-</div>
-
-</footer>
-</html>
