9fd7bc0a00f0d5c3f74a216766139176fc817ab6
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-15 08:30:58 +0000

    added: burger menu for resposive design

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,4 +12,5 @@
                     </ul>
                 </nav>
                 <img src="./static/images/cart.png" width="40px" />
+                <img src="./static/images/menu.png" class="menu-icon" />
             </div>
