033ec908019a2b1483c656025cca5280640696f5
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-17 12:50:37 +0000

    added: admin dashboard to manage products

---
+++ admin/dashboard.php
@@ -0,0 +1,77 @@
+    <!-- Header -->
+    <?php require_once('../config.php') ?>
+    <!-- Header -->
+
+    <!-- Header -->
+    <?php include_once('../includes/header.php'); ?>
+    <!-- Header -->
+
+<body>
+    <div class="header">
+        <div class="container">
+            <!-- Navbar -->
+            <?php include_once('./includes/admin-navbar.php'); ?>
+            <!-- Navbar -->
+        </div>
+    </div>
+
+            <!-- Product Board -->
+                <?php
+                        $query= mysqli_query( $conn ,"SELECT * FROM products");
+                        while($row=mysqli_fetch_array($query))
+                 {
+                    $id=$row['id'];
+                    $name=$row["name"];
+                    $price=$row["price"];
+                    $picture=$row["picture"];
+                    $description= substr($row["description"],0,200);
+                    $date=$row["created_at"];
+
+
+                ?>
+
+                <div class="row">
+                    <div class="col-4">
+                        <?php echo "<img src='../static/images/".$row['picture']."' >"; ?>
+                        <h4><?php echo "$name";  ?></h4>
+                            <p>$<?php echo "$price";  ?></p>
+                            <p><?php echo "$description";  ?></p>
+                        <a href=""><i class="fas fa-trash"></i></a>
+                        <a href=""><i class="fas fa-edit"></i></a>
+                    </div>
+                </div>
+                <?php } ?>
+            <!-- Product Board -->
+
+</body>
+
+    <!-- Footer -->
+    <?php include_once('../includes/footer.php'); ?>
+    <!-- Footer -->
+
+    <!-- Scripts -->
+
+       <!-- Burger Menu -->
+
+       <script>
+           var MenuItems = document.getElementById("MenuItems");
+
+           MenuItems.style.maxHeight = "0px";
+
+           function menutoggle(){
+               if(MenuItems.style.maxHeight == "0px")
+               {
+                MenuItems.style.maxHeight = "200px";
+               }
+               else
+               {
+                MenuItems.style.maxHeight = "0px";
+               }
+           }
+       </script>
+
+       <!-- Burger Menu -->
+
+    <!-- Scripts -->
+
+
