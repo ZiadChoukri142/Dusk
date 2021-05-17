2c5f6d334e12502fdf51be3285ae0d7db8953702
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-17 11:33:02 +0000

    modified: connection method to Object-Oriented

--- config.php
+++ config.php
@@ -6,15 +6,13 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-
-    try {
-    $conn = new PDO("mysql:host=$servername;dbname=ecommerce_site", $username, $password);
-        
-    // verify connection
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}       catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+    $dbname = "ecommerce_site";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
 
 ?>
