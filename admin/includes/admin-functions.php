ad07c1f7f44038248d00b256630834594f04686e
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-19 11:17:43 +0000

    added: category insert function

--- admin/includes/admin-functions.php
+++ admin/includes/admin-functions.php
@@ -1,5 +1,6 @@
 <?php
   // *** INSERT DATA ***
+    // ** PRODUCT **
 
   // Create database connection
   $db = mysqli_connect("localhost", "root", "", "ecommerce_site");
@@ -7,15 +8,15 @@
   // Initialize message variable
   $msg = "";
 
-  // If upload button is clicked ...
-  if (isset($_POST['upload'])) {
-  	// Get image name
+    // submit button clicked
+    if (isset($_POST['upload_product'])) {
+  	// get data names
     $name = $_POST['name'];
     $price = $_POST['price'];
     $image = $_FILES['image']['name'];
     $description = $_POST['description'];
     $category_id = $_POST['category_id'];
-
+    
   	// image file directory
   	$target = "./static/images/".basename($image);
 
@@ -24,11 +25,40 @@
   	mysqli_query($db, $sql);
 
   	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-  		$msg = "Image uploaded successfully";
+  		$msg = "Product uploaded successfully";
   	}else{
-  		$msg = "Failed to upload image";
+  		$msg = "Failed to upload Product";
   	}
   }
   $result = mysqli_query($db, "SELECT * FROM products");
+
+?>
+
+<?php
+  // *** INSERT DATA ***
+    // ** CATEGORY **
+    
+  // Create database connection
+  $db = mysqli_connect("localhost", "root", "", "ecommerce_site");
+
+  // Initialize message variable
+  $msg = "";
+
+    // submit button clicked
+    if (isset($_POST['upload_category'])) {
+  	// get data names
+    $category_name = $_POST['category_name'];
+
+  	$sql = "INSERT INTO categories (name) VALUES ('$category_name')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+    if(mysqli_query($db,$sql)) {
+  		$msg = "Product uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload Product";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM categories");
 
 ?>
