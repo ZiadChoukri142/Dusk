c2d1098133434eb7e194eb98a77a5d11855e7b94
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-19 19:41:47 +0000

    added: animation script for login / reg form

--- account.php
+++ account.php
@@ -34,18 +34,18 @@
                <div class="col-2">
                   <div class="form-container">
                      <div class="form-btn">
-                        <span>Login</span>
-                        <span>Register</span>
-                        <hr id="indicator">
+                        <span onclick="login()">Login</span>
+                        <span onclick="register()">Register</span>
+                        <hr id="Indicator">
                      </div>
 
-                     <form id="loginForm">
+                     <form id="LoginForm">
                         <input type="text" placeholder="Username">
                         <input type="password" placeholder="Password">
                         <button type="submit" class="btn">Login</button>
                         <a href="">Forgot Password</a>
                      </form>
-                     <form id="regForm">
+                     <form id="RegForm">
                         <input type="text" placeholder="Username">
                         <input type="email" placeholder="Email">
                         <input type="password" placeholder="Password">
@@ -84,6 +84,29 @@
       }
    </script>
    <!-- Burger Menu -->
+
+   <!-- Form Toggle -->
+
+      <script>
+         var LoginForm = document.getElementById("LoginForm");
+         var RegForm = document.getElementById("RegForm");
+         var Indicator = document.getElementById("Indicator");
+
+            function register(){
+               RegForm.style.transform = "translateX(0px)";
+               LoginForm.style.transform = "translateX(0px)";
+               Indicator.style.transform = "translateX(100px)";
+            }
+
+            function login(){
+               RegForm.style.transform = "translateX(300px)";
+               LoginForm.style.transform = "translateX(300px)";
+               Indicator.style.transform = "translateX(0px)";
+            }
+
+      </script>
+
+   <!-- Form Toggle -->
 
    <!-- Scripts -->
 </body>
