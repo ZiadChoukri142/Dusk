98d078de4128659fdb3b5ca5aa300eb83470ac52
ZiadChoukri142 <ziad.choukrii@gmail.com>
2021-05-21 01:27:42 +0000

    fixed: function bug -> code reworked

--- admin/includes/admin-functions.php
+++ admin/includes/admin-functions.php
@@ -1,23 +1,36 @@
 <?php require_once('../config.php') ?>
-<?php  
+
+<?php
 
 // ********** ADD NEW PRODUCT **********
 
-if (isset($_POST['upload_product'])) {
+  $msg = "";
   
-    $name        = $_POST['name'];
-    $price       = $_POST['price'];
-    $description = $_POST['description'];
+  // If upload button is clicked ...
+  if (isset($_POST['upload_product'])) {
+
+    $name        = $_POST["name"];
+    $price       = $_POST["price"];
+    $description = $_POST["description"];
+    $category_id = $_POST["category_id"];
     $filename    = $_FILES["image"]["name"];
     $tempname    = $_FILES["image"]["tmp_name"];    
     $folder      = "../static/images/".$filename;
           
     $conn = mysqli_connect("localhost", "root", "", "ecommerce_site");
   
-        $sql = "INSERT INTO products (name,price,picture,description) VALUES ('$name', '$price','$filename', '$description')";
+        // Get all the submitted data from the form
+        $sql = "INSERT INTO products (name, price, picture, description, category_id) VALUES ('$name', '$price', '$filename', '$description', '$category_id')";
   
+        // Execute query
         mysqli_query($conn, $sql);
-
+          
+        // Now let's move the uploaded image into the folder: image
+        if (move_uploaded_file($tempname, $folder))  {
+            $msg = "Image uploaded successfully";
+        }else{
+            $msg = "Failed to upload image";
+      }
   }
   $result = mysqli_query($conn, "SELECT * FROM products");
 ?>
@@ -26,19 +39,30 @@ if (isset($_POST['upload_product'])) {
 
 // ********** ADD NEW CATEGORY **********
 
+  $msg = "";
+  
+  // If upload button is clicked ...
   if (isset($_POST['upload_category'])) {
 
-    $name        = $_POST['category_name'];
-    $filename    = $_FILES["category_image"]["name"];
-    $tempname    = $_FILES["category_image"]["tmp_name"];    
-    $folder      = "../static/images/".$filename;
+    $name     = $_POST["category_name"];
+    $filename = $_FILES["category_image"]["name"];
+    $tempname = $_FILES["category_image"]["tmp_name"];    
+    $folder   = "../static/images/".$filename;
           
-        $conn = mysqli_connect("localhost", "root", "", "ecommerce_site");
-      
-        $sql = "INSERT INTO categories (name,picture) VALUES ('$name','$filename')";
+    $conn = mysqli_connect("localhost", "root", "", "ecommerce_site");
   
+        // Get all the submitted data from the form
+        $sql = "INSERT INTO categories (name, picture) VALUES ('$name', '$filename')";
+  
+        // Execute query
         mysqli_query($conn, $sql);
-      
+          
+        // Now let's move the uploaded image into the folder: image
+        if (move_uploaded_file($tempname, $folder))  {
+            $msg = "Image uploaded successfully";
+        }else{
+            $msg = "Failed to upload image";
+      }
   }
   $result = mysqli_query($conn, "SELECT * FROM categories");
 ?>
