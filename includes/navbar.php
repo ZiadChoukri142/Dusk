da1915c025319e1adbdaf2ef068d650b87e03518
ZiadChoukri <ziad.choukrii@gmail.com>
2021-05-24 22:45:53 +0000

    modified: file location

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar">
    <div class="logo">
-      <a href="../index.php"><img src="./static/images/brand.png" width="130px" /></a>
+      <a href="../index.php"><img src="../static/images/brand.png" width="130px" /></a> 
    </div>
    <nav>
       <ul id="MenuItems">
@@ -8,9 +8,9 @@
          <li><a href="../products.php">Products</a></li>
          <li><a href="">About</a></li>
          <li><a href="">Contact</a></li>
-         <li><a href="../account.php">Account</a></li>
       </ul>
    </nav>
-   <a href="../cart.php"><img src="./static/images/cart.png" width="40px" /></a>
+   <a href="../cart.php"><img src="../static/images/cart.png" width="40px" /></a>
    <img src="./static/images/menu.png" class="menu-icon" onclick="menutoggle()" />
 </div>
+
