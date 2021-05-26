622095c50dcf655a65f767b227841035de463686
ZiadChoukri <ziad.choukrii@gmail.com>
2021-05-26 01:23:40 +0000

    added: fully functional cart page

--- cart.php
+++ cart.php
@@ -1,103 +1,147 @@
+<?php include ('./includes/header.php'); ?>
+<div class="container">
+<?php include ('./includes/navbar.php'); ?>
+</div>
+
+<br><br><br><br>
 <?php 
-  session_start(); 
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "ecommerce_site");
 
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
+if(isset($_POST["add_to_cart"]))
+{
+	if(isset($_SESSION["shopping_cart"]))
+	{
+		$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+		if(!in_array($_GET["id"], $item_array_id))
+		{
+			$count = count($_SESSION["shopping_cart"]);
+			$item_array = array(
+				'item_id'			=>	$_GET["id"],
+				'item_name'			=>	$_POST["hidden_name"],
+				'item_price'		=>	$_POST["hidden_price"],
+				'item_quantity'		=>	$_POST["quantity"]
+			);
+			$_SESSION["shopping_cart"][$count] = $item_array;
+		}
+		else
+		{
+			echo '<script>alert("Item Already Added")</script>';
+		}
+	}
+	else
+	{
+		$item_array = array(
+			'item_id'			=>	$_GET["id"],
+			'item_name'			=>	$_POST["hidden_name"],
+			'item_price'		=>	$_POST["hidden_price"],
+			'item_quantity'		=>	$_POST["quantity"]
+		);
+		$_SESSION["shopping_cart"][0] = $item_array;
+	}
+}
+
+if(isset($_GET["action"]))
+{
+	if($_GET["action"] == "delete")
+	{
+		foreach($_SESSION["shopping_cart"] as $keys => $values)
+		{
+			if($values["item_id"] == $_GET["id"])
+			{
+				unset($_SESSION["shopping_cart"][$keys]);
+				echo '<script>alert("Item Removed")</script>';
+				echo '<script>window.location="cart.php"</script>';
+			}
+		}
+	}
+}
+
 ?>
-<!-- Functions -->
-<?php
-   require_once('./includes/functions.php');
-   ?>
-<!-- Functions -->
-<!-- Config File -->
-<?php
-   require_once('config.php');
-   ?>
-<!-- Config File -->
-<!-- Header -->
-<?php
-   include_once('./includes/header.php');
-   ?>
-<!-- Header -->
-<body>
-      <div class="container">
-   <!-- Navbar -->
-   <?php
-      include_once('./includes/navbar.php');
-      ?>
-      </div>
 
-   <!-- Navbar -->
 
-   <!-- Cart -->
+		<div class="small-container">
+         <div class="row">
+			<?php
+				$query = "SELECT * FROM products ORDER BY id ASC";
+				$result = mysqli_query($connect, $query);
+				if(mysqli_num_rows($result) > 0)
+				{
+					while($row = mysqli_fetch_array($result))
+					{
+				?>
+            
+			<div class="col-4">
+				<form method="post" action="cart.php?action=add&id=<?php echo $row["id"]; ?>">
+					<div style="border:3px solid #33174e; background-color:whitesmoke; border-radius:5px; padding:16px;" align="center">
+						<img src="../static/images/<?php echo $row["picture"]; ?>" /><br />
 
-         <!-- Items -->
+						<h4><?php echo $row["name"]; ?></h4>
 
-      <div class="small-container cart-page">
-         <table>
-            <tr>
-               <th>Product</th>
-               <th>Quantity</th>
-               <th>Subtotal</th>
-            </tr>
-            <tr>
-               <td>
-                  <div class="cart-info">
-                     <img src="../static/images/buy-1.jpg">
-                     <div>
-                        <p>Red Printed T-Shirt</p>
-                        <small>Price: $50.00</small>
-                        <br>
-                        <a href="">Remove</a>
-                     </div>
-                  </div>
-               </td>
-               <td><input type="number" value="1" min="1"></td>
-               <td>$50.00</td>
-            </tr>
-         </table>
+						<h4>$ <?php echo $row["price"]; ?></h4>
 
-            <!-- Items -->
+						<input type="text" name="quantity" value="1" class="form-control" />
 
-            <!-- Total -->
+						<input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
 
-         <div class="total-price">
-            <table>
-               <tr>
-                  <td>SubTotal</td>
-                  <td>$200.00</td>
-               </tr>
-               <tr>
-                  <td>Tax</td>
-                  <td>$35.00</td>
-               </tr>
-               <tr>
-               <td>Total</td>
-               <td>$235.00</td>
-               </tr>
-            </table>
+						<input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+
+						<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn" value="Add to Cart" />
+
+					
+				</form>
+			</div>
          </div>
-
-            <!-- Total -->
-
-
-      </div>
-
-   <!-- Cart -->
-
-   <!-- Footer -->
-   <?php
-      include_once('./includes/footer.php');
-      ?>
-   <!-- Footer -->
-   <!-- Scripts -->
+			<?php
+					}
+				}
+			?>
+         </div>
+			<div></div>
+			<br />
+			<h3>Order Details</h3>
+			<div>
+				<table>
+					<tr>
+						<th width="40%">Item Name</th>
+						<th width="10%">Quantity</th>
+						<th width="20%">Price</th>
+						<th width="15%">Total</th>
+						<th width="5%">Action</th>
+					</tr>
+					<?php
+					if(!empty($_SESSION["shopping_cart"]))
+					{
+						$total = 0;
+						foreach($_SESSION["shopping_cart"] as $keys => $values)
+						{
+					?>
+					<tr>
+						<td><?php echo $values["item_name"]; ?></td>
+						<td><?php echo $values["item_quantity"]; ?></td>
+						<td>$ <?php echo $values["item_price"]; ?></td>
+						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
+						<td><a href="cart.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span>Remove</span></a></td>
+					</tr>
+					<?php
+							$total = $total + ($values["item_quantity"] * $values["item_price"]);
+						}
+					?>
+					<tr>
+						<td colspan="3" align="right">Total</td>
+						<td align="right">$ <?php echo number_format($total, 2); ?></td>
+						<td></td>
+					</tr>
+					<?php
+					}
+					?>
+						
+				</table>
+			</div>
+		</div>
+	</div>
+	<br />
+      <!-- Scripts -->
    <!-- Burger Menu -->
    <script>
       var MenuItems = document.getElementById("MenuItems");
@@ -117,4 +161,6 @@
    </script>
    <!-- Burger Menu -->
    <!-- Scripts -->
-</body>
+	</body>
+
+
