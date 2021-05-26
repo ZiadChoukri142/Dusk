daea02d9b71523d5ead4f83b9e105acdd2dd880a
ZiadChoukri <ziad.choukrii@gmail.com>
2021-05-26 01:26:00 +0000

    added: ajax CDN

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="../static/css/public_style.css" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
       <link rel="preconnect" href="https://fonts.g.com" />
       <link
          href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
