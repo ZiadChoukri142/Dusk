72eeb5b7b791e4fe6f388eccfe0d8bcb702a5310
ZiadChoukri <ziad.choukrii@gmail.com>
2021-05-29 09:38:36 +0000

    removed: header tag

--- register.php
+++ register.php
@@ -1,10 +1,7 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+<?php include ('./includes/header.php'); ?>
 <body>
    <style>
    * {
@@ -99,7 +96,7 @@ form, .content {
   	  <input type="password" name="password_2" required>
   	</div>
   	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	  <button type="submit" class="btn" name="reg_user" style="width: 35%;">Register</button>
   	</div>
   	<p>
   		Already a member? <a href="login.php">Sign in</a>
